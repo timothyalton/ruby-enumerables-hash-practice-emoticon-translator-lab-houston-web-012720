1fec655f2b2d0cecf8e7948330d9c9c60651239e
Mitchell T Alton <mitchell.alton@live.com>
2020-01-25 20:20:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,7 +10,7 @@ def load_library(file_path)
   emoticon_file.each do |meaning, value|
     english = value[0]
     japanese = value[1]
-    new_hash["get_meaning"]
+    new_hash["get_meaning"][]
   end 
   new_hash
 end
